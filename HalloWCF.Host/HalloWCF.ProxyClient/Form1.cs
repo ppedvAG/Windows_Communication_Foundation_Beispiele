e3af7534db85251719fd6991863363c2ab76129e
André <ruland@outlook.de>
2020-06-03 11:50:53 +0200

    ProxyClient für HalloWCF Host

---
+++ HalloWCF.Host/HalloWCF.ProxyClient/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HalloWCF.ProxyClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var client = new ServiceReference1.Service1Client();
+            dataGridView1.DataSource = client.GetAllPizza();
+        }
+    }
+}
