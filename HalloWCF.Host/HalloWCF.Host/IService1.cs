0e736b01c3503d1861d0605acf4f40240a82e903
André <ruland@outlook.de>
2020-06-03 11:14:29 +0200

    Einfacher WCF Host

---
+++ HalloWCF.Host/HalloWCF.Host/IService1.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace HalloWCF.Host
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        int GetFive();
+
+        [OperationContract]
+        int Sum(int a, int b);
+
+        [OperationContract]
+        IEnumerable<Pizza> GetAllPizza();
+
+        [OperationContract]
+        Drink GetOneDrink();
+    }
+
+    
+    public class Drink
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public string Name { get; set; }
+        public bool Alc { get; set; }
+    }
+
+    [DataContract]
+    public class Pizza
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember(Name = "Price")]
+        public decimal Preis { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        public int NurIntern { get; set; }
+    }
+}
---
+++ HalloWCF.Host/HalloWCF.Host/Service1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace HalloWCF.Host
+{
+    public class Service1 : IService1
+    {
+        public IEnumerable<Pizza> GetAllPizza()
+        {
+            yield return new Pizza() { Id = 1, Name = "Salami", Preis = 12.8m };
+            yield return new Pizza() { Id = 2, Name = "Schinken", Preis = 16.2m };
+            yield return new Pizza() { Id = 6, Name = "Hawai", Preis = 6.2m };
+        }
+
+        public int GetFive()
+        {
+            return 5;
+        }
+
+        public Drink GetOneDrink()
+        {
+            return new Drink() { Id = 5, Name = "Jim Beam", Price = 4m, Alc = true };
+        }
+
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
