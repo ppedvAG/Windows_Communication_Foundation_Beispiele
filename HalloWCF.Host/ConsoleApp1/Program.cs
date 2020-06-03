a083076bc81f52599dbb1ebe9b9b2c6c03681db4
André <ruland@outlook.de>
2020-06-03 12:06:27 +0200

    WPF .net Core Client

---
+++ HalloWCF.Host/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using ServiceReference1;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+         static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var client = new Service1Client(Service1Client.EndpointConfiguration.NetTcpBinding_IService1);
+            foreach (var item in await client.GetAllPizzaAsync())
+            {
+                Console.WriteLine(item.Name);
+            } 
+            
+
+
+            Console.WriteLine("Ende");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HalloWCF.Host/CoreWPFClient/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using ServiceReference1;
+using System.Windows;
+
+namespace CoreWPFClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Lade(object sender, RoutedEventArgs e)
+        {
+            var client = new Service1Client(Service1Client.EndpointConfiguration.BasicHttpBinding_IService1);
+            myGrid.ItemsSource = await client.GetAllPizzaAsync();
+        }
+
+    }
+}
